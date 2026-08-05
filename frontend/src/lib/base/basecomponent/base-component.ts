eefada316c81e8ee644759544f4044382f4cc86d
Oğuzhan Girgin <oguzhan_girgin155@hotmail.com>
2026-08-05 23:23:35 +0300

    routes and lib structure

---
+++ frontend/src/lib/base/basecomponent/base-component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+})
+export class BaseComponent {}
---
+++ frontend/src/lib/commons/body/body.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-body',
+  imports: [],
+  templateUrl: './body.html',
+  styleUrl: './body.scss',
+})
+export class Body {}
