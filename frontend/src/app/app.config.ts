876f7ef421f6e263d8977298876146f567155044
Oğuzhan Girgin <oguzhan_girgin155@hotmail.com>
2026-08-06 22:26:02 +0300

    env and scss

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -6,12 +6,15 @@ import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { AppModule } from './app-module';
 import { BaseInterceptor } from '../lib/base/baseinterceptor/baseinterceptor';
+import { provideApi } from '../lib/services/provide-api';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes), provideClientHydration(withEventReplay()),
     provideHttpClient(withFetch(), withInterceptors([BaseInterceptor])),
+    provideApi(environment.apiUrl),
     importProvidersFrom(AppModule)
   ]
 };
--- frontend/src/lib/base/baseinterceptor/baseinterceptor.ts
+++ frontend/src/lib/base/baseinterceptor/baseinterceptor.ts
@@ -1,6 +1,7 @@
 import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { catchError, finalize, throwError } from 'rxjs';
+import { environment } from '../../../environments/environment';
 import { FlowService } from '../baseservice/flowservice';
 
 export const BaseInterceptor: HttpInterceptorFn = (request, next) => {
@@ -12,7 +13,7 @@ export const BaseInterceptor: HttpInterceptorFn = (request, next) => {
     headers['Accept'] = 'application/json';
   }
 
-  headers['Accept-Language'] = flowService.get<string>('language') ?? 'tr';
+  headers['Accept-Language'] = flowService.get<string>('language') ?? environment.defaultLanguage;
 
   const token = flowService.token();
   
